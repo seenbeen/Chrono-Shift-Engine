f3f7cfad10a954665bbbcb21bef8093fe8870abc
Shiyang Han <s55han@uwaterloo.ca>
2017-06-12 03:19:40 -0400

    GLM Added in, refactored project directory

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <asset/image/image.h>
 #include <asset/shader/shaders.h>
