335f3f2cb0272856c7bd481b68b4540be6d6046b
seenbeen <s55han@uwaterloo.ca>
2017-08-06 05:51:49 -0400

    fixing some measurements real quick

--- src/Experimental/Test1/teststage.cpp
+++ src/Experimental/Test1/teststage.cpp
@@ -28,11 +28,11 @@ namespace Experimental { namespace Test1 {
         this->viewport->bindCamera(this->camera);
 
         // gulp...
-        CSELL::Math::Vector3f pos = CSELL::Math::Vector3f(-0.5f, 0.5f, -2.0f);
+        CSELL::Math::Vector3f pos = CSELL::Math::Vector3f(-200.0f, 150.0f, -2.0f);
         this->testObject1 = new Experimental::Test1::TestGameObject(this->scene, pos);
-        pos += CSELL::Math::Vector3f(0.5f, -0.5f, 2.0f);
+        pos += CSELL::Math::Vector3f(200.0f, -150.0f, 2.0f);
         this->testObject2 = new Experimental::Test1::TestGameObject(this->scene, pos);
-        pos -= CSELL::Math::Vector3f(-0.5f, 0.5f, 1.0f);
+        pos -= CSELL::Math::Vector3f(-200.0f, 150.0f, 1.0f);
         this->testObject3 = new Experimental::Test1::TestGameObject(this->scene, pos);
 
         this->addObject(this->testObject1);
